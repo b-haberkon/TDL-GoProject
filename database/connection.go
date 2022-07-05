8ddbb8ac77beb2b7de5f3a01eb5624a57ffa1ab5
b-haberkon <bhaberkon@fi.uba.ar>
2022-07-05 02:11:35 -0300

    imagenes, data, carga en la db

--- database/connection.go
+++ database/connection.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	"fmt"
+	"bufio"
+	"os"
 	"system/models"
-
 	//"gorm.io/driver/mysql"
 	//"gorm.io/driver/sqlite"
 	"github.com/glebarez/sqlite"
@@ -25,11 +27,36 @@ func conn_to_sqlite() (db *gorm.DB, err error) {
 
 func Connect() {
 	connection, err := conn_to_sqlite()
+	defer database.DBConn.Close()
 
 	if err != nil {
 		panic("could not connection to database")
 	}
 
 	DB = connection
-	connection.AutoMigrate(&models.User{})
+	connection.AutoMigrate(&models.User{}, &models.Data{})
+
+	CargarDatos()
+}
+
+func CargarDatos() {
+	archivo, error := os.Open("./../memotest_data.txt")
+	defer archivo.Close()
+	if error != nil {
+		panic("error opening file")
+	}
+
+	scanner := bufio.NewScanner(archivo)
+
+	for scanner.Scan(){
+		linea := scanner.Text()
+		info := strings.Split(linea, ",")
+
+		var data Data 
+		data.Name = info[0]
+		data.Imagen = info[1]
+		data.Japones = info[2]
+
+		DB.Create(&data)
+	}
 }
